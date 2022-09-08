612aebde9fdf0185a077532dcbe26b97b3ea63ee
Simon Prickett <simon@redislabs.com>
2022-09-08 13:30:58 +0100

    Updated with code from livestream.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import network
-import time
 import secrets
+import time
 from picoredis import Redis
+
+REDIS_HOST = "redis-11565.c275.us-east-1-4.ec2.cloud.redislabs.com"
+REDIS_PORT = 11565
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -11,7 +14,16 @@ while not wlan.isconnected() and wlan.status() >= 0:
     print("Connecting to wifi...")
     time.sleep(0.5)
 
-r = Redis(host="192.168.4.24", port=6379)
+# Should have a network connection...
 
-r.lpush('pi_pico', 'hello')
-r.xadd('astream', '*', 'sender', 'pi_pico', 'data', 'todo')
+# Connect to Redis...
+r = Redis(host = REDIS_HOST, port = REDIS_PORT)
+r.auth(secrets.REDIS_PASSWORD)
+
+n = 0
+while True:
+    # Add some value to a Redis stream... 
+    stream_id = r.xadd("picoproject:incoming", "*", "device_id", "1", "data", str(n))
+    print(stream_id)
+    time.sleep(2)
+    n = n + 1
