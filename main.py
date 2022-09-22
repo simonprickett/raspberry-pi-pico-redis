021074c8bcd4d7b9c47b6897fe80ff1138862e92
Simon Prickett <simon@redislabs.com>
2022-09-22 11:22:05 +0100

    Adds temperature/humidity code.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import network
 import secrets
 import time
 from picoredis import Redis
+from dht11 import *
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -13,14 +14,20 @@ while not wlan.isconnected() and wlan.status() >= 0:
 
 # Should have a network connection...
 
+# Set up the temperature/humidity sensor.
+temp_humidity_sensor = DHT(18)
+
 # Connect to Redis...
 r = Redis(host = secrets.REDIS_HOST, port = secrets.REDIS_PORT)
 r.auth(secrets.REDIS_PASSWORD)
 
-n = 0
 while True:
-    # Add some value to a Redis stream... 
-    stream_id = r.xadd("picoproject:incoming", "*", "device_id", "1", "data", str(n))
+    # Read the temperature and humidity data from the sensor...
+    temperature, humidity = temp_humidity_sensor.readTempHumid()
+
+    # Send temperature and humidity data to a Redis stream... 
+    stream_id = r.xadd("picoproject:incoming", "*", "id", "1", "t", str(temperature), "h", str(humidity))
     print(stream_id)
+    print(temperature)
+    print(humidity)
     time.sleep(2)
-    n = n + 1
