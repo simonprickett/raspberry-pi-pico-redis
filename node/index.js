4d8ab963d916dff1aed1b4eca4f18269078cbc3b
Simon Prickett <simon@redislabs.com>
2022-10-06 11:16:35 +0100

    Added memo functionality.

--- node/index.js
+++ node/index.js
@@ -5,13 +5,19 @@ const REDIS_PORT = process.env.REDIS_PORT;
 const REDIS_USER = process.env.REDIS_USER || 'default';
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
+const LAST_ID_KEY = 'picoproject:consumer:last_id';
+
 const client = createClient({
   url: `redis://${REDIS_USER}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/`
 });
 
 await client.connect();
 
-let currentId = '0-0'; // Start at lowest possible stream ID
+let currentId = await client.get(LAST_ID_KEY);
+
+if (! currentId) {
+  currentId = '0-0';
+}
 
 while (true) {
   try {
@@ -38,6 +44,7 @@ while (true) {
       // Get the ID of the first (only) entry returned.
       currentId = response[0].messages[0].id;
       console.log(currentId);
+      client.set(LAST_ID_KEY, currentId);
     } else {
       // Response is null, we have read everything that is
       // in the stream right now...
