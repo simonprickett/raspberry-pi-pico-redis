02ca406ef081d1c8d6a97476093a97be8f35d9a4
Simon Prickett <simon@redislabs.com>
2022-10-06 11:01:13 +0100

    Added light sensor.

--- micropython/main.py
+++ micropython/main.py
@@ -3,6 +3,7 @@ import secrets
 import time
 from picoredis import Redis
 from dht11 import *
+from machine import ADC
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -17,6 +18,9 @@ while not wlan.isconnected() and wlan.status() >= 0:
 # Set up the temperature/humidity sensor.
 temp_humidity_sensor = DHT(18)
 
+# Set up the light sensor.
+light_sensor = ADC(1)
+
 # Connect to Redis...
 r = Redis(host = secrets.REDIS_HOST, port = secrets.REDIS_PORT)
 r.auth(secrets.REDIS_PASSWORD)
@@ -25,9 +29,13 @@ while True:
     # Read the temperature and humidity data from the sensor...
     temperature, humidity = temp_humidity_sensor.readTempHumid()
 
+    # Read the light sensor...
+    light = light_sensor.read_u16()
+
     # Send sensor data to a Redis stream... 
-    stream_id = r.xadd("picoproject:incoming", "*", "id", "1", "t", str(temperature), "h", str(humidity))
+    stream_id = r.xadd("picoproject:incoming", "*", "id", "1", "t", str(temperature), "h", str(humidity), "l", str(light))
     print(f"id: {stream_id}")
     print(f"t: {temperature}")
-    print(f"h {humidity}")
+    print(f"h: {humidity}")
+    print(f"l: {light}")
     time.sleep(2)
